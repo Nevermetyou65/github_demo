356bd2d4cdab4313be1cda9591a72c5b9ac8db81
PAKPOOM SINGKORAPOOM <pakpoom.sig@cpf.co.th>
2022-07-01 17:00:49 +0700

    add feature a

---
+++ feature_a.py
@@ -0,0 +1,10 @@
+def print_name_and_age():
+    
+    name = input('input your name:')
+    age = input('input your age:')
+    
+    print(f'{name}, {age}')
+    
+
+if __name__ == '__main__':
+    print_name_and_age()
